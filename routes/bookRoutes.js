be882763688940ab89380a93500f06ebfa02b798
Amina Meirman <amina20050307@gmail.com>
2024-02-23 06:56:47 +0600

    Added logging to routes in bookRoutes.js

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -3,8 +3,24 @@ const router = express.Router();
 const bookController = require('../controllers/bookController');
 const { validateBook, validate } = require('../utils/validation');
 
-router.get('/', bookController.getAllBooks);
-router.post('/add', validateBook(), validate, bookController.addBook);
-router.put('/:id/update', validateBook(), validate, bookController.updateBook);
-router.delete('/:id/delete', bookController.deleteBook);
+router.get('/', (req, res, next) => {
+    console.log(`[${new Date().toISOString()}] INFO: Route: GET /books, IP: ${req.ip}`);
+    next();
+}, bookController.getAllBooks);
+
+router.post('/add', validateBook(), validate, (req, res, next) => {
+    console.log(`[${new Date().toISOString()}] INFO: Route: POST /books/add, IP: ${req.ip}`);
+    next();
+}, bookController.addBook);
+
+router.put('/:id/update', validateBook(), validate, (req, res, next) => {
+    console.log(`[${new Date().toISOString()}] INFO: Route: PUT /books/${req.params.id}/update, IP: ${req.ip}`);
+    next();
+}, bookController.updateBook);
+
+router.delete('/:id/delete', (req, res, next) => {
+    console.log(`[${new Date().toISOString()}] INFO: Route: DELETE /books/${req.params.id}/delete, IP: ${req.ip}`);
+    next();
+}, bookController.deleteBook);
+
 module.exports = router;
